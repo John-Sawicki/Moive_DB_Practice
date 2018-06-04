8aa4d9e4f290b6c0d41b540a8d9a75ac75762611
john-sawicki <3Gxapnil>
2018-06-03 20:25:31 -0500

    can add second movie and delete db

--- app/src/main/java/com/example/android/moivedbpractice/AddValuesToDB.java
+++ app/src/main/java/com/example/android/moivedbpractice/AddValuesToDB.java
@@ -1,4 +1,17 @@
 package com.example.android.moivedbpractice;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+
 public class AddValuesToDB {
+
+    public AddValuesToDB(SQLiteDatabase db, Context context){
+        FavoriteDbHelper dbHelper = new FavoriteDbHelper(context);
+        db=dbHelper.getWritableDatabase();
+    }
+    public void getMovieValues(String[] movieValues){
+
+    }
 }
--- app/src/main/java/com/example/android/moivedbpractice/MainActivity.java
+++ app/src/main/java/com/example/android/moivedbpractice/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.android.moivedbpractice;
 
 import android.content.ContentValues;
+import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
@@ -14,24 +15,32 @@ import android.widget.Toast;
 import com.example.android.moivedbpractice.FavoriteMovieContract.FavoriteMovieEntry;
 public class MainActivity extends AppCompatActivity {
     public SQLiteDatabase mDb;
-    private Button addButton, queryButton,addSecondMovie;
+    private Button addButton, queryButton,addSecondMovie, deleteButton;
+    String[] movieTwo = {"random url", "inception", "2010", "8.9", "matrix inside a matrix","4568465"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FavoriteDbHelper dbHelper = new FavoriteDbHelper(getApplicationContext());
+        mDb = dbHelper.getWritableDatabase();
         addButton = findViewById(R.id.add_button);
         queryButton = findViewById(R.id.query_button);
         addSecondMovie = findViewById(R.id.add_second_movie);
+        deleteButton = findViewById(R.id.delete_button);
         Log.d("onCreate", "onCreate");
-
+        Context context = getApplicationContext();
+        AddValuesToDB addValues = new AddValuesToDB(mDb, context);
+       /*
+        if(mDb!=null){
+            mDb.delete(FavoriteMovieEntry.TABLE_NAME, null, null);
+            Log.d("delete"," in delete if statement");
+        }
+*/
         addButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 //TODO add generic static text to query
-
-                FavoriteDbHelper dbHelper = new FavoriteDbHelper(getApplicationContext());
-                mDb=dbHelper.getWritableDatabase();
-
                 ContentValues cv = new ContentValues();
                 cv.put(FavoriteMovieEntry.POSTER_PATH, "random url");
                 cv.put(FavoriteMovieEntry.ORIGINAL_TITLE, "The Matrix");
@@ -41,18 +50,24 @@ public class MainActivity extends AppCompatActivity {
                 cv.put(FavoriteMovieEntry.ID, "3141592657");
                 Log.d("query click", "added all content values");
                 mDb.insert(FavoriteMovieEntry.TABLE_NAME, null, cv);
-                mDb.close();
-                /*
-                Uri uri = getContentResolver().insert(FavoriteMovieEntry.CONTENT_URI, cv);
-                if(uri!=null){
-                    Toast.makeText(getBaseContext(), uri.toString(), Toast.LENGTH_SHORT).show();
-                }
-                */
+                //mDb.close();
+
             }
         });
         addSecondMovie.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                ContentValues cv = new ContentValues();
+                cv.put(FavoriteMovieEntry.POSTER_PATH, movieTwo[0]);
+                cv.put(FavoriteMovieEntry.ORIGINAL_TITLE, movieTwo[1]);
+                cv.put(FavoriteMovieEntry.RELEASE_DATE, movieTwo[2]);
+                cv.put(FavoriteMovieEntry.VOTE_AVERAGE, movieTwo[3]);
+                cv.put(FavoriteMovieEntry.OVERVIEW, movieTwo[4]);
+                cv.put(FavoriteMovieEntry.ID, movieTwo[5]);
+                Log.d("query click", "added all second content values");
+                mDb.insert(FavoriteMovieEntry.TABLE_NAME, null, cv);
+               // mDb.close();
 
             }
         });
@@ -63,28 +78,48 @@ public class MainActivity extends AppCompatActivity {
                 //FavoriteDbHelper dbHelper = new FavoriteDbHelper(getApplicationContext());
                 //dbHelper.getReadableDatabase();
                 FavoriteDbHelper dbHelper = new FavoriteDbHelper(getApplicationContext());
-                mDb=dbHelper.getReadableDatabase();
+                mDb = dbHelper.getReadableDatabase();
                 Cursor cursor = getAllMovies();
                 //recyclerViewAdapter = new adapter(context, cursor.getCount());    //only add as many elements as are in the cursor/ db
-                if(cursor.moveToFirst()){
-                    String movieTitle = cursor.getString(cursor.getColumnIndex(FavoriteMovieEntry.ORIGINAL_TITLE));
-                    Log.d("query", movieTitle);
+                if (cursor.moveToFirst()) {
+                    for (int i = 0; i < cursor.getCount(); i++) {
+                        Log.d("cursor", cursor.getCount() + "");
+                        String movieTitle = cursor.getString(cursor.getColumnIndex(FavoriteMovieEntry.ORIGINAL_TITLE));
+                        Log.d("query", movieTitle);
+                    }
+
                 }
                 Log.d("query", "end of onClick");
 
-
-
+            }
+        });
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mDb.delete(FavoriteMovieEntry.TABLE_NAME,null, null);
+                /*
+                Uri uri = getContentResolver().insert(FavoriteMovieEntry.CONTENT_URI, cv);
+                if(uri!=null){
+                    Toast.makeText(getBaseContext(), uri.toString(), Toast.LENGTH_SHORT).show();
+                }
+                */
             }
         });
     }
+
     private Cursor getAllMovies(){
         return mDb.query(
                 FavoriteMovieEntry.TABLE_NAME,
                 null,
-                null,
+                null, //only query movie based on year release, matrix is 1999
                 null,
                 null,
                 null,
                 null);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
     }
 }
